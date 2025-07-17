3788b5c6f1d91d5d1b21656b3d99c52bc02e24a0
Ethan <52676614+ethanelliott50@users.noreply.github.com>
2025-07-17 11:12:14 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 import os
 from nsepython import nse_optionchain_scrapper
+from nsepython import derivative_history
 from typing import Optional
 import requests
 
@@ -43,12 +44,20 @@ async def get_cpi(region: str = Query(...), data: str = Query(...), year: Option
     }
     
 # Indian Options Chain
-@app.get("/options")
-async def get_options(symbol: str = Query(...)):
+@app.get("/options_chain")
+async def options_chain(symbol: str = Query(...)):
     try:
         url = f"https://raw.githubusercontent.com/ethanelliott50/NSE/main/{symbol.lower()}_option_chain.json"
         response = requests.get(url)
         response.raise_for_status()
         return response.json()
+    except Exception as e:
+        return {"error": str(e)}
+        
+@app.get("/historical")
+async def historical(symbol: str = Query(...), start_date: str = Query(...), end_date: str = Query(...), expiry_date: str = Query(...), strikePrice: int = Query(...), optionType: str = Query(...)):
+    try:
+        df = derivative_history(symbol, start_date, end_date, "options", expiry_date, strikePrice, optionType)
+        return df.to_dict(orient="records")
     except Exception as e:
         return {"error": str(e)}
